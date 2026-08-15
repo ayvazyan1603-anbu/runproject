ac41b99d63db4f677f22035cd89567c4ec7688b2
ayvazyan1603-anbu <ayvazyan1603@gmail.com>
2026-08-15 19:12:44 +0400

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,3 +1,6 @@
 import { createStart } from "@tanstack/react-start";
+import { getRouter } from "./router";
 
-export const startInstance = createStart();
+export const startInstance = createStart({
+  getRouter,
+});
